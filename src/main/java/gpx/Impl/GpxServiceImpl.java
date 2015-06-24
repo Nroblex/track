a80d8ed4c8205a1e4a967b6ea89091005955bc37
Anders (ueh093) <anders.selborn@teliasonera.com>
2015-06-24 15:45:26 +0200

    Adding support for Documentbuilder

--- src/main/java/gpx/Impl/GpxServiceImpl.java
+++ src/main/java/gpx/Impl/GpxServiceImpl.java
@@ -1,17 +1,27 @@
 package gpx.Impl;
 
 import gpx.GpxService;
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -41,8 +51,57 @@ public class GpxServiceImpl extends ImportEngine implements GpxService {
                 // Converting uploaded file to inputstream.
                 InputStream inputStream = inputPart.getBody(InputStream.class, null);
 
-                byte[] bytes = IOUtils.toByteArray(inputStream);
-                saveGPX(bytes);
+                //byte[] bytes = IOUtils.toByteArray(inputStream);
+
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                docFactory.setNamespaceAware(true);
+
+                DocumentBuilder documentBuilder=null;
+
+                try {
+                    documentBuilder= docFactory.newDocumentBuilder();
+
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Document document = documentBuilder.parse(inputStream);
+
+                    XPath xPath = XPathFactory.newInstance().newXPath();
+                    xPath.setNamespaceContext(new NamespaceContext() {
+
+                        public String getNamespaceURI(String prefix) {
+                            if (prefix == null) throw new NullPointerException("Null Prefix");
+                            else if ("sh".equals(prefix))
+                                return "http://www.topografix.com/GPX/1/1";
+                            else if ("sh".equals(prefix))
+                                return XMLConstants.XML_NS_URI;
+
+                            return XMLConstants.XML_NS_URI;
+                        }
+
+                        public String getPrefix(String namespaceURI) {
+                            if (namespaceURI.equals("http://www.topografix.com/GPX/1/1"))
+                                return "sh";
+
+                            return null;
+                        }
+
+                        public Iterator getPrefixes(String namespaceURI) {
+                            ArrayList list = new ArrayList<String>();
+
+                            if (namespaceURI.equals("http://www.topografix.com/GPX/1/1"))
+                                list.add("sh");
+                            return null;
+                        }
+                    });
+
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+
+                //saveGPX(bytes);
 
                 // fileName = "/home/ueh093/" + fileName;
                 // writeFile(bytes, fileName);
